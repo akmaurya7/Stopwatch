3afa16f12f1525ac3095cc6b65945d79ac7b038d
akmaurya7 <amaurya4570@gmail.com>
2024-02-28 20:27:48 +0530

    Layout for all the pages are completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,9 @@ plugins {
 android {
     namespace = "com.example.stopwatch"
     compileSdk = 34
+    buildFeatures{
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "com.example.stopwatch"
