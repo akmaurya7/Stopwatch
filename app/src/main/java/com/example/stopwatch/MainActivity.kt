d10f9846893d08bb86aa1b701fb4bffaa5234ebd
akmaurya7 <amaurya4570@gmail.com>
2024-02-29 18:20:06 +0530

    create the dialog popup

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.stopwatch
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
+import com.example.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.IVTimerImage.setOnClickListener {
+            var dialog = Dialog(this)
+            dialog.setContentView(R.layout.dialog)
+            var numberPicker = dialog.findViewById<NumberPicker>(R.id.NPNumberPicker)
+            numberPicker.minValue =0
+            numberPicker.maxValue = 5
+            dialog.show()
+        }
     }
 }
