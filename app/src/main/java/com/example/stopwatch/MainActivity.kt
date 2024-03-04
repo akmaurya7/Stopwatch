38fcd370f5a17ff7c05beba986ddd8e6300cc4bb
akmaurya7 <amaurya4570@gmail.com>
2024-03-04 23:39:31 +0530

    Dialog box now working and time is set

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.stopwatch
 
+import android.app.AlertDialog
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.NumberPicker
 import com.example.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    val binding by lazy{
+    val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,11 +17,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.IVTimerImage.setOnClickListener {
             var dialog = Dialog(this)
-            dialog.setContentView(R.layout.dialog)
-            var numberPicker = dialog.findViewById<NumberPicker>(R.id.NPNumberPicker)
-            numberPicker.minValue =0
+            dialog.setContentView(R.layout.dialogbox)
+            var numberPicker = dialog.findViewById<NumberPicker>(R.id.numberPicker)
+            numberPicker.minValue = 0
             numberPicker.maxValue = 5
+            dialog.findViewById<Button>(R.id.setTimeButton).setOnClickListener {
+                binding.TVSetTime.text = dialog.findViewById<NumberPicker>(R.id.numberPicker).value.toString() +" min"
+                dialog.dismiss()
+            }
             dialog.show()
         }
+
+
     }
+
 }
